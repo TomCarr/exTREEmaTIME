4d9af6f9bc1566f703ae874d8fe2eda991b3edd3
TomCarr <tomcarruthers94@gmail.com>
2020-12-02 11:51:13 +0000

    Add files via upload

--- R/exTREEmaTIME_SetAutoRates.R
+++ R/exTREEmaTIME_SetAutoRates.R
@@ -3,7 +3,7 @@
 #' @export
 SetAutoRates <- function(tree, root_min, root_max, unit){
 
-rmax <- (max(node.depth.edgelength(tree)[seq(1, length(tree$tip.label), 1)])*unit)/(root_min + ((root_max-root_min)/2))
-rmin <- (min(node.depth.edgelength(tree)[seq(1, length(tree$tip.label), 1)])/unit)/(root_min + ((root_max-root_min)/2))
+rmax <<- (max(node.depth.edgelength(tree)[seq(1, length(tree$tip.label), 1)])*unit)/(root_min + ((root_max-root_min)/2))
+rmin <<- (min(node.depth.edgelength(tree)[seq(1, length(tree$tip.label), 1)])/unit)/(root_min + ((root_max-root_min)/2))
 
 }
