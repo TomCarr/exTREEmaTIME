5afb3395dd4758b692c19773c03256ab979c61dd
TomCarr <tomcarruthers94@gmail.com>
2021-07-03 15:22:17 +0100

    Delete exTREEmaTIME_SetAutoRates.R

--- R/exTREEmaTIME_SetAutoRates.R
+++
@@ -1,9 +0,0 @@
-#' calculates plausible values for rmax and rmin for subsequent use in UncertainTree
-#' increase the unit until UncertainTreeCalc reaches completion
-#' @export
-SetAutoRates <- function(tree, root_min, root_max, unit){
-
-rmax <<- (max(node.depth.edgelength(tree)[seq(1, length(tree$tip.label), 1)])*unit)/(root_min + ((root_max-root_min)/2))
-rmin <<- (min(node.depth.edgelength(tree)[seq(1, length(tree$tip.label), 1)])/unit)/(root_min + ((root_max-root_min)/2))
-
-}
